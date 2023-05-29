0f74b8599fb0cd7a9e42dc37bc6252aabbd3ad9d
marinakreibring <timarina1997@yahoo.com>
2023-05-29 22:12:10 +0300

    lazy loading lesson 7

---
+++ lesson7/script.js
@@ -0,0 +1,36 @@
+//script for loading images
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+//my option for intersection observer
+const imgOpt = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+//check for intersection observer
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imgOpt);
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+} 
+else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    }, );
+} 
+//script for footer
+document.querySelector("#currYear").textContent = new Date().getFullYear();
+document.querySelector("#updated").textContent = document.lastModified;
