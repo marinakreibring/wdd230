52f7491c830b471bdc55616370ec0b733173d354
marinakreibring <timarina1997@yahoo.com>
2023-06-04 07:50:27 +0300

    discover page for chamber site

---
+++ chamber/scripts/visits.js
@@ -0,0 +1,16 @@
+//localStorage.clear(); // this line is to check how it works from the beginning
+const currentDate = new Date(); 
+const lastVisitDate = localStorage.getItem('lastVisitDate'); 
+let daysSinceLastVisit;
+
+if (lastVisitDate) {
+  const timeSinceLastVisit = currentDate.getTime() - new Date(lastVisitDate).getTime();
+  daysSinceLastVisit = Math.floor(timeSinceLastVisit / (1000 * 60 * 60 * 24)); // value in brackets - milliseconds per day
+  if (daysSinceLastVisit == 0){
+    daysSinceLastVisit = `You've visited today`;
+  };
+} else {
+  daysSinceLastVisit = `This is your first visit`;
+}
+document.getElementById("visits").innerHTML = daysSinceLastVisit;
+//localStorage.setItem('lastVisitDate', currentDate.toISOString()); 
