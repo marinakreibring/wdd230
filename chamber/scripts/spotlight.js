e29fc7c91f74887af593e72445df6951a91c0874
marinakreibring <timarina1997@yahoo.com>
2023-07-01 17:35:20 +0300

    dynamic spotlight

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,81 +1,71 @@
+//the list of businesses with gold level membership
 let goldList = [
     {
-        "name": "Empolis GmbH",        
-        "motto": "Fast problem solving with computing and enterprise AI",
-        "phone": "+49 631680370",
-        "web": "https://www.empolis.com",
-        "logo": "images/business/empolis-logo.png",
-        "level": "gold"           
+        name: "Empolis GmbH",        
+        motto: "Fast problem solving with computing and enterprise AI",
+        phone: "+49 631680370",
+        web: "https://www.empolis.com",
+        logo: "images/business/empolis-logo.png"        
     },
     {
-        "name": "FK Kraemer GmbH",
-        "motto": "We train YOU in civil engineering and road building!",
-        "phone": "+49 63154154",
-        "web": "https://www.fk-kraemer.de",
-        "logo": "images/business/kramer-logo.png",
-        "level": "gold"           
+        name: "FK Kraemer GmbH",
+        motto: "We train YOU in civil engineering and road building",
+        phone: "+49 63154154",
+        web: "https://www.fk-kraemer.de",
+        logo: "images/business/kramer-logo.png"          
     },
     {
-        "name": "Human Solutions GmbH",
-        "motto": "We make ergonomics digital and your vehicles better",
-        "phone": "+49 631343593",
-        "web": "https://www.human-solutions.com",
-        "logo": "images/business/HumSol-logo.png",
-        "level": "gold"         
+        name: "Human Solutions GmbH",
+        motto: "We make ergonomics digital and your vehicles better",
+        phone: "+49 631343593",
+        web: "https://www.human-solutions.com",
+        logo: "images/business/HumSol-logo.png"       
     },
     {
-        "name": "WIPOTEC GmbH",
-        "motto": "Innovation and passion for technology",
-        "phone": "+49 631341460",
-        "web": "https://www.wipotec.com",
-        "logo": "images/business/wipotec-logo.png",
-        "level": "gold"             
+        name: "WIPOTEC GmbH",
+        motto: "Innovation and passion for technology",
+        phone: "+49 631341460",
+        web: "https://www.wipotec.com",
+        logo: "images/business/wipotec-logo.png"         
     }
-]
-
-
-
-
-
-
-
-
-//add spotlights to div
-const addSpotlight = (business) => {
-    const spotlight = document.querySelector("div.spotlight");
-    business.forEach((item) => {
-        //find gold level
-        if (item.level == "gold"){
-            
-        }
-
-
-        let spot = document.createElement("section");
-        let logo = document.createElement("img");
-        let name = document.createElement("h3");
-        let address1 = document.createElement("p");
-        let address2 = document.createElement("p");
-        let phone = document.createElement("p");
-        let web = document.createElement("a");
-
-        logo.setAttribute("src", item.logo);
-        logo.setAttribute("alt", `Logo of ${item.name}`);
-        logo.setAttribute("loading", "lazy");        
-        
-        name.textContent = `${item.name}`;
-        address1.textContent = `${item.address[0]}`;
-        address2.textContent = `${item.address[1]}`;
-        phone.textContent = `${item.phone}`;
-        web.innerHTML = `${item.web}`;            
-        web.setAttribute("href", `${item.web}`);
-        card.appendChild(name);
-        card.appendChild(logo);
-        card.appendChild(address1);
-        card.appendChild(address2);
-        card.appendChild(phone);
-        card.appendChild(web);
-        
-        spotlight.appendChild(spot);
-    });
-}
+];
+//function to generate random number in given range
+function getRandomBetween(min, max) {
+    return Math.floor(min + Math.random()*(max - min + 1))
+};
+//choose random index between second and prelast, and then use +1/-1 
+const i = getRandomBetween(1, goldList.length-2);
+const j = i-1;
+const h = i+1;
+//fill spans for elements in the spotlight section
+//for spotlight 1
+let logo1 = document.querySelector("#logo1");
+logo1.setAttribute("src", `${goldList[i].logo}`);
+logo1.setAttribute("alt", `Logo of ${goldList[i].name}`);
+logo1.setAttribute("loading", "lazy"); 
+document.querySelector("#motto1").textContent = `${goldList[i].motto}`;
+document.querySelector("#tel1").textContent = `${goldList[i].phone}`;
+let web1 = document.querySelector("#web1");
+web1.innerHTML = `${goldList[i].web}`;            
+web1.setAttribute("href", `${goldList[i].web}`);
+//for spotlight 2
+let logo2 = document.querySelector("#logo2");
+logo2.setAttribute("src", `${goldList[j].logo}`);
+logo2.setAttribute("alt", `Logo of ${goldList[j].name}`);
+logo2.setAttribute("loading", "lazy"); 
+document.querySelector("#motto2").textContent = `${goldList[j].motto}`;
+document.querySelector("#tel2").textContent = `${goldList[j].phone}`;
+let web2 = document.querySelector("#web2");
+web2.innerHTML = `${goldList[j].web}`;            
+web2.setAttribute("href", `${goldList[j].web}`);
+//for spotlight 3
+let logo3 = document.querySelector("#logo3");
+logo3.setAttribute("src", `${goldList[h].logo}`);
+logo3.setAttribute("alt", `Logo of ${goldList[h].name}`);
+logo3.setAttribute("loading", "lazy"); 
+document.querySelector("#motto3").textContent = `${goldList[h].motto}`;
+document.querySelector("#tel3").textContent = `${goldList[h].phone}`;
+let web3 = document.querySelector("#web3");
+web3.innerHTML = `${goldList[h].web}`;            
+web3.setAttribute("href", `${goldList[h].web}`);
 
